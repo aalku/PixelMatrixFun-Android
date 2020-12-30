62cfbab6207900093bf68f9b0291d76f31e61e8a
aalku <aalku7@gmail.com>
2020-12-30 17:57:55 +0100

    Bitmap finger draw. Not on PixelMatrix.

--- PixelMatrixFun/app/src/main/java/org/aalku/pixelmatrixfun/DrawView.java
+++ PixelMatrixFun/app/src/main/java/org/aalku/pixelmatrixfun/DrawView.java
@@ -1,74 +1,89 @@
 package org.aalku.pixelmatrixfun;
 
 import android.content.Context;
-import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.text.TextPaint;
+import android.graphics.Rect;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
+import androidx.annotation.RequiresApi;
+
 /**
- * TODO: document your custom view class.
+ * Bitmap to draw on
  */
 public class DrawView extends View {
-    private TextPaint mTextPaint;
-    private float mTextWidth;
-    private float mTextHeight;
+    private final Bitmap bitmap;
+    private int imageWidth = 16;
+    private int imageHeight = 16;
+    private Rect paintRect;
 
     public DrawView(Context context) {
-        super(context);
-        init(null, 0);
+        this(context, null);
     }
 
     public DrawView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(attrs, 0);
+        bitmap = Bitmap.createBitmap(imageWidth, imageHeight, Bitmap.Config.ARGB_8888);
     }
 
-    public DrawView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init(attrs, defStyle);
-    }
-
-    private void init(AttributeSet attrs, int defStyle) {
-        // Set up a default TextPaint object
-        mTextPaint = new TextPaint();
-        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setTextAlign(Paint.Align.LEFT);
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements();
-    }
-
-    private void invalidateTextPaintAndMeasurements() {
-        mTextPaint.setTextSize(32);
-        mTextPaint.setColor(Color.DKGRAY);
-        mTextWidth = mTextPaint.measureText("ASDFG");
-
-        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-        mTextHeight = fontMetrics.bottom;
-    }
-
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        int paddingLeft = getPaddingLeft();
-        int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
-        int paddingBottom = getPaddingBottom();
-
-        int contentWidth = getWidth() - paddingLeft - paddingRight;
-        int contentHeight = getHeight() - paddingTop - paddingBottom;
-
-        // Draw the text.
-        canvas.drawText("ASDFG",
-                paddingLeft + (contentWidth - mTextWidth) / 2,
-                paddingTop + (contentHeight + mTextHeight) / 2,
-                mTextPaint);
+        canvas.drawBitmap(bitmap, null, this.paintRect, null);
     }
 
+    private void drawPixel(int x, int y, int width, int height) {
+        int rx = Math.max(Math.min(imageWidth*x/width,imageHeight - 1), 0);
+        int ry = Math.max(Math.min(imageHeight*y/height, imageHeight - 1), 0);
+        Log.d("DRAW", String.format("[%s,%s] out of [%s,%s]", rx, ry, bitmap.getWidth(), bitmap.getHeight()));
+        bitmap.setPixel(rx, ry, Color.WHITE);
+        invalidate();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        for (int p = 0; p < event.getPointerCount(); p++) {
+            float x = event.getX(p);
+            float y = event.getY(p);
+
+            switch(event.getAction()) {
+                case MotionEvent.ACTION_DOWN :
+                    drawPixel((int)x, (int)y, this.getWidth(), this.getHeight());
+                case MotionEvent.ACTION_MOVE :
+                    drawPixel((int)x, (int)y, this.getWidth(), this.getHeight());
+                case MotionEvent.ACTION_UP :
+                    //touchUp();
+                    invalidate();
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        if (changed) {
+            int paddingLeft = getPaddingLeft();
+            int paddingTop = getPaddingTop();
+            int paddingRight = getPaddingRight();
+            int paddingBottom = getPaddingBottom();
+
+            int contentWidth = getWidth() - paddingLeft - paddingRight;
+            int contentHeight = getHeight() - paddingTop - paddingBottom;
+            this.paintRect = new Rect(0, 0, contentWidth, contentHeight);
+        }
+
+        super.onLayout(changed, left, top, right, bottom);
+    }
 }
