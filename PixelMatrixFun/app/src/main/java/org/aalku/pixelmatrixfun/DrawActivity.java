263c2fb9157c798d8ce656fa655d077d5e74100d
aalku <aalku7@gmail.com>
2020-12-31 14:56:38 +0100

    Status text on drawing.

--- PixelMatrixFun/app/src/main/java/org/aalku/pixelmatrixfun/DrawActivity.java
+++ PixelMatrixFun/app/src/main/java/org/aalku/pixelmatrixfun/DrawActivity.java
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.function.Consumer;
 
 public class DrawActivity extends AppCompatActivity {
 
     private Consumer<String> statusListener = s->setStatusText(s);
+    private TextView statusText;
+    private DeviceService deviceService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        deviceService = DeviceService.instance;
         setContentView(R.layout.activity_draw);
-        DeviceService.instance.addStatusListener(statusListener);
+        statusText = this.findViewById(R.id.statusText);
+        deviceService.addStatusListener(statusListener);
+        setStatusText(deviceService.getStatusText());
     }
 
     @Override
@@ -26,7 +33,7 @@ public class DrawActivity extends AppCompatActivity {
 
     protected void setStatusText(String msg) {
         Log.i("STATUS", msg);
-        // this.runOnUiThread(()->statusText.setText(msg));
+        this.runOnUiThread(()->statusText.setText(msg));
     }
 
 }
